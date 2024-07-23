043ca20fa9f3bf17951132808978e55551c32b0d
Seb Viret <s.viret@ipnl.in2p3.fr>
2024-07-23 14:56:29 +0200

    analysis update

--- analyze_precess.py
+++ analyze_precess.py
@@ -1,45 +1,12 @@
 '''
-Small macro to retrieve a template from a bank and compare it to another template
 
-Usage:
-
-python plot_temp.py A B
-
-with:
-A: rank of the template in the bank
-B: name of the bank file (pickle file)
 '''
 
-
-from scipy.stats import pearsonr
-from numpy import load
-import matplotlib.pyplot as plt
-import pandas as pd 
-import pickle
 import sys
 import ROOT as root
 from time import time
-import gen_template as gt
 import numpy as np
-from scipy.spatial import distance
-from scipy.signal import correlate
 from array import array
-
-def readFilePrecess(fname):    # Parser of the file containing the parameters we want to test
-
-    nbcoals=[]
-    masses=[]
-    spins=[]
-    with open(fname,"r") as f:
-        for line in f:
-            if line.startswith('#'):
-                continue
-            value=[float(v) for v in line.split(',')]
-            
-            nbcoals.append(value[0])
-            masses.append(value[1:3])
-            spins.append(value[3:9])
-    return nbcoals,masses,spins
 
 # The main routine
 
@@ -47,8 +14,16 @@ def main():
     import gendata as gd
     args = sys.argv[1:] # Get input params
     
-    # Retrieve the bank (do this once)
+    # Retrieve the root file name
     fname=args[0]
+    # How many injections 
+    ninj=int(args[1])
+
+    fparts=fname.split('/')
+    basename=fparts[len(fparts)-1]
+
+
+    f = open(f"filter_{basename.split('root')[0]}txt", "w")
 
     file = root.TFile.Open(fname,"READ")
 
@@ -62,10 +37,11 @@ def main():
     chieff_p  = array('d', [0])
     chip_p    = array('d', [0])
     mf        = array('d', [0])
+    cross     = array('d', [0])
     rk_b      = array('d', [0])
     rk_p      = array('d', [0])
 
-    file2  = root.TFile.Open('filter.root', 'recreate')
+    file2  = root.TFile.Open(f"filter_{basename.split('root')[0]}root", 'recreate')
 
     tree2 = root.TTree("comp", "comp")
     tree2.Branch("mchirp_b",     Mc_b,     'Mc_b/D')
@@ -75,20 +51,22 @@ def main():
     tree2.Branch("chieff_b", chieff_b,     'chieff_b/D')
     tree2.Branch("chieff_p", chieff_p,     'chieff_p/D')
     tree2.Branch("chip_p",     chip_p,     'chip_p/D')
-    tree2.Branch("mf", mf,     'mf/D')
-    tree2.Branch("rk_b",           rk_b,     'rk_b/D')
-    tree2.Branch("rk_p",           rk_p,     'rk_p/D')
+    tree2.Branch("mf",             mf,     'mf/D')
+    tree2.Branch("cross",       cross,     'cross/D')
+    tree2.Branch("rk_b",         rk_b,     'rk_b/D')
+    tree2.Branch("rk_p",         rk_p,     'rk_p/D')
 
-
-    mvals=np.zeros(5000)
-    for i in range(5000):
-        mvals[i]=-1
-    bkvals=np.zeros(5000)
+    mvals=np.full((ninj),-1.)
+    bkvals=np.zeros(ninj)
     ent=0
 
     for entry in tree3:
+        if ent%100000==0:
+            print(ent)
         rk=int(tree3.rk_p)
-        rb=tree3.rk_b
+        if rk>=ninj: 
+            ent+=1
+            continue
         mfr=tree3.mf
         if mfr>mvals[rk]:
             mvals[rk]=mfr
@@ -96,10 +74,9 @@ def main():
         ent+=1
         
 
-    for i in range(4500):
-        if (mvals[i]==-1):
+    for i in range(ninj):
+        if (mvals[i]<=0.):
             continue
-
         idx=int(bkvals[i])
         tree3.GetEntry(idx)
         Mc_p[0]=tree3.Mc_p
@@ -111,14 +88,14 @@ def main():
         q_b[0]=tree3.q_b
         chieff_b[0]=tree3.chieff_b
         rk_b[0]=tree3.rk_b
-        if (mvals[i]/np.sqrt(2)<0.5):
-            print(int(tree3.rk_p),int(tree3.rk_b),mvals[i]/np.sqrt(2),tree3.Mc_p)
+        f.write(f"{int(tree3.rk_p)},{int(tree3.rk_b)},{mvals[i]},{tree3.Mc_p}\n")
         mf[0]=mvals[i]
+        cross[0]=tree3.cross
         tree2.Fill()
 
     file2.Write()
     file2.Close()
-
+    f.close()
 
 
 
